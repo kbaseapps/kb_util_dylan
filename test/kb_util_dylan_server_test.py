7f325c373850db224a8433df9686213cac60dde5
dcchivian <dcchivian@lbl.gov>
2017-07-02 23:11:43 -0700

    passing first test

--- test/kb_util_dylan_server_test.py
+++ test/kb_util_dylan_server_test.py
@@ -3,16 +3,17 @@ import os
 import json
 import time
 import requests
+import shutil
 
 from os import environ
 from ConfigParser import ConfigParser
 from requests_toolbelt import MultipartEncoder
 from pprint import pprint
 
-from biokbase.workspace.client import Workspace as workspaceService
+from Workspace.WorkspaceClient import Workspace as workspaceService
 from biokbase.AbstractHandle.Client import AbstractHandle as HandleService
 from kb_util_dylan.kb_util_dylanImpl import kb_util_dylan
-
+from ReadsUtils.ReadsUtilsClient import ReadsUtils
 
 class kb_util_dylanTest(unittest.TestCase):
 
@@ -28,13 +29,14 @@ class kb_util_dylanTest(unittest.TestCase):
         config = ConfigParser()
         config.read(config_file)
         for nameval in config.items('kb_util_dylan'):
+            print(nameval[0] + '=' + nameval[1])
             cls.cfg[nameval[0]] = nameval[1]
         cls.wsURL = cls.cfg['workspace-url']
-        cls.wsClient = workspaceService(cls.wsURL, token=token)
-        cls.serviceImpl = kb_util_dylan(cls.cfg)
-
         cls.shockURL = cls.cfg['shock-url']
         cls.handleURL = cls.cfg['handle-service-url']
+        cls.serviceWizardURL = cls.cfg['service-wizard-url']
+        cls.wsClient = workspaceService(cls.wsURL, token=token)
+        cls.serviceImpl = kb_util_dylan(cls.cfg)
 
 
     @classmethod
@@ -42,36 +44,17 @@ class kb_util_dylanTest(unittest.TestCase):
         if hasattr(cls, 'wsName'):
             cls.wsClient.delete_workspace({'workspace': cls.wsName})
             print('Test workspace was deleted')
+        if hasattr(cls, 'shock_ids'):
+            for shock_id in cls.shock_ids:
+                print('Deleting SHOCK node: '+str(shock_id))
+                cls.delete_shock_node(shock_id)
 
-        
     @classmethod
-    def upload_file_to_shock(cls, file_path):
-        """
-        Use HTTP multi-part POST to save a file to a SHOCK instance.
-        """
-
-        header = dict()
-        header["Authorization"] = "Oauth {0}".format(cls.token)
-
-        if file_path is None:
-            raise Exception("No file given for upload to SHOCK!")
-
-        with open(os.path.abspath(file_path), 'rb') as dataFile:
-            files = {'upload': dataFile}
-            response = requests.post(
-                cls.shockURL + '/node', headers=header, files=files,
-                stream=True, allow_redirects=True, timeout=30)
-
-        if not response.ok:
-            response.raise_for_status()
-
-        result = response.json()
-
-        if result['error']:
-            raise Exception(result['error'][0])
-        else:
-            return result["data"]
-
+    def delete_shock_node(cls, node_id):
+        header = {'Authorization': 'Oauth {0}'.format(cls.token)}
+        requests.delete(cls.shockURL + '/node/' + node_id, headers=header,
+                        allow_redirects=True)
+        print('Deleted shock node ' + node_id)
 
     def getWsClient(self):
         return self.__class__.wsClient
@@ -85,90 +68,6 @@ class kb_util_dylanTest(unittest.TestCase):
         self.__class__.wsName = wsName
         return wsName
 
-    # call this method to get the WS object info of a Paired End Library (will
-    # upload the example data if this is the first time the method is called during tests)
-    def getPairedEndLibInfo(self):
-        if hasattr(self.__class__, 'pairedEndLibInfo'):
-            return self.__class__.pairedEndLibInfo
-        # 1) upload files to shock
-        token = self.ctx['token']
-        forward_shock_file = self.upload_file_to_shock('data/small.forward.fq')
-        reverse_shock_file = self.upload_file_to_shock('data/small.reverse.fq')
-        #pprint(forward_shock_file)
-        #pprint(reverse_shock_file)
-
-        # 2) create handle
-        hs = HandleService(url=self.handleURL, token=token)
-        forward_handle = hs.persist_handle({
-                                        'id' : forward_shock_file['id'], 
-                                        'type' : 'shock',
-                                        'url' : self.shockURL,
-                                        'file_name': forward_shock_file['file']['name'],
-                                        'remote_md5': forward_shock_file['file']['checksum']['md5']}
-)
-
-        reverse_handle = hs.persist_handle({
-                                        'id' : reverse_shock_file['id'], 
-                                        'type' : 'shock',
-                                        'url' : self.shockURL,
-                                        'file_name': reverse_shock_file['file']['name'],
-                                        'remote_md5': reverse_shock_file['file']['checksum']['md5']})
-
-        # 3) save to WS
-        paired_end_library = {
-            'lib1': {
-                'file': {
-                    'hid':forward_handle,
-                    'file_name': forward_shock_file['file']['name'],
-                    'id': forward_shock_file['id'],
-                    'url': self.shockURL,
-                    'type':'shock',
-                    'remote_md5':forward_shock_file['file']['checksum']['md5']
-                },
-                'encoding':'UTF8',
-                'type':'fastq',
-                'size':forward_shock_file['file']['size']
-            },
-            'lib2': {
-                'file': {
-                    'hid':reverse_handle,
-                    'file_name': reverse_shock_file['file']['name'],
-                    'id': reverse_shock_file['id'],
-                    'url': self.shockURL,
-                    'type':'shock',
-                    'remote_md5':reverse_shock_file['file']['checksum']['md5']
-                },
-                'encoding':'UTF8',
-                'type':'fastq',
-                'size':reverse_shock_file['file']['size']
-
-            },
-            'interleaved':0,
-            'sequencing_tech':'artificial reads'
-        }
-
-        new_obj_info = self.wsClient.save_objects({
-                        'workspace':self.getWsName(),
-                        'objects':[
-                            {
-                                'type':'KBaseFile.PairedEndLibrary',
-                                'data':paired_end_library,
-                                'name':'test.pe.reads',
-                                'meta':{},
-                                'provenance':[
-                                    {
-                                        'service':'MEGAHIT',
-                                        'method':'test_megahit'
-                                    }
-                                ]
-                            }]
-                        })
-        self.__class__.pairedEndLibInfo = new_obj_info[0]
-        return new_obj_info[0]
-
-
-
-
     def getImpl(self):
         return self.__class__.serviceImpl
 
@@ -176,18 +75,226 @@ class kb_util_dylanTest(unittest.TestCase):
         return self.__class__.ctx
 
 
+    # call this method to get the WS object info of a Single End Library (will
+    # upload the example data if this is the first time the method is called during tests)
+    def getSingleEndLibInfo(self, read_lib_basename, lib_i=0):
+        if hasattr(self.__class__, 'singleEndLibInfo_list'):
+            try:
+                info = self.__class__.singleEndLibInfo_list[lib_i]
+                name = self.__class__.singleEndLibName_list[lib_i]
+                if info != None:
+                    if name != read_lib_basename:
+                        self.__class__.singleEndLib_SetInfo[lib_i] = None
+                        self.__class__.singleEndLib_SetName[lib_i] = None
+                    else:
+                        return info
+            except:
+                pass
+
+        # 1) upload files to shock
+        shared_dir = "/kb/module/work/tmp"
+        forward_data_file = 'data/'+read_lib_basename+'.fwd.fq'
+        forward_file = os.path.join(shared_dir, os.path.basename(forward_data_file))
+        shutil.copy(forward_data_file, forward_file)
+
+        ru = ReadsUtils(os.environ['SDK_CALLBACK_URL'])
+        single_end_ref = ru.upload_reads({'fwd_file': forward_file,
+                                          'sequencing_tech': 'artificial reads',
+                                          'wsname': self.getWsName(),
+                                          'name': 'test-'+str(lib_i)+'.se.reads'})['obj_ref']
+
+        new_obj_info = self.getWsClient().get_object_info_new({'objects': [{'ref': single_end_ref}]})[0]
+
+        # store it
+        if not hasattr(self.__class__, 'singleEndLibInfo_list'):
+            self.__class__.singleEndLibInfo_list = []
+            self.__class__.singleEndLibName_list = []
+        for i in range(lib_i+1):
+            try:
+                assigned = self.__class__.singleEndLibInfo_list[i]
+            except:
+                self.__class__.singleEndLibInfo_list.append(None)
+                self.__class__.singleEndLibName_list.append(None)
+
+        self.__class__.singleEndLibInfo_list[lib_i] = new_obj_info
+        self.__class__.singleEndLibName_list[lib_i] = read_lib_basename
+        return new_obj_info
+
+
+    # call this method to get the WS object info of a Paired End Library (will
+    # upload the example data if this is the first time the method is called during tests)
+    def getPairedEndLibInfo(self, read_lib_basename, lib_i=0):
+        if hasattr(self.__class__, 'pairedEndLibInfo_list'):
+            try:
+                info = self.__class__.pairedEndLibInfo_list[lib_i]
+                name = self.__class__.pairedEndLibName_list[lib_i]
+                if info != None:
+                    if name != read_lib_basename:
+                        self.__class__.pairedEndLibInfo_list[lib_i] = None
+                        self.__class__.pairedEndLibName_list[lib_i] = None
+                    else:
+                        return info
+            except:
+                pass
+
+        # 1) upload files to shock
+        shared_dir = "/kb/module/work/tmp"
+        forward_data_file = 'data/'+read_lib_basename+'.fwd.fq'
+        forward_file = os.path.join(shared_dir, os.path.basename(forward_data_file))
+        shutil.copy(forward_data_file, forward_file)
+        reverse_data_file = 'data/'+read_lib_basename+'.rev.fq'
+        reverse_file = os.path.join(shared_dir, os.path.basename(reverse_data_file))
+        shutil.copy(reverse_data_file, reverse_file)
+
+        ru = ReadsUtils(os.environ['SDK_CALLBACK_URL'])
+        paired_end_ref = ru.upload_reads({'fwd_file': forward_file, 'rev_file': reverse_file,
+                                          'sequencing_tech': 'artificial reads',
+                                          'interleaved': 0, 'wsname': self.getWsName(),
+                                          'name': 'test-'+str(lib_i)+'.pe.reads'})['obj_ref']
+
+        new_obj_info = self.getWsClient().get_object_info_new({'objects': [{'ref': paired_end_ref}]})[0]
+
+        # store it
+        if not hasattr(self.__class__, 'pairedEndLibInfo_list'):
+            self.__class__.pairedEndLibInfo_list = []
+            self.__class__.pairedEndLibName_list = []
+        for i in range(lib_i+1):
+            try:
+                assigned = self.__class__.pairedEndLibInfo_list[i]
+            except:
+                self.__class__.pairedEndLibInfo_list.append(None)
+                self.__class__.pairedEndLibName_list.append(None)
+
+        self.__class__.pairedEndLibInfo_list[lib_i] = new_obj_info
+        self.__class__.pairedEndLibName_list[lib_i] = read_lib_basename
+        return new_obj_info
+
+
+    # call this method to get the WS object info of a Single End Library Set (will
+    # upload the example data if this is the first time the method is called during tests)
+    def getSingleEndLib_SetInfo(self, read_libs_basename_list, refresh=False):
+        if hasattr(self.__class__, 'singleEndLib_SetInfo'):
+            try:
+                info = self.__class__.singleEndLib_SetInfo
+                if info != None:
+                    if refresh:
+                        self.__class__.singleEndLib_SetInfo = None
+                    else:
+                        return info
+            except:
+                pass
+
+        # build items and save each SingleEndLib
+        items = []
+        for lib_i,read_lib_basename in enumerate (read_libs_basename_list):
+            label    = read_lib_basename
+            lib_info = self.getSingleEndLibInfo (read_lib_basename, lib_i)
+            lib_ref  = str(lib_info[6])+'/'+str(lib_info[0])
+            print ("LIB_REF["+str(lib_i)+"]: "+lib_ref+" "+read_lib_basename)  # DEBUG
+
+            items.append({'ref': lib_ref,
+                          'label': label
+                          #'data_attachment': ,
+                          #'info':
+                         })
+
+        # save readsset
+        desc = 'test ReadsSet'
+        readsSet_obj = { 'description': desc,
+                         'items': items
+                       }
+        name = 'TEST_READSET'
+
+        new_obj_info = self.wsClient.save_objects({
+                        'workspace':self.getWsName(),
+                        'objects':[
+                            {
+                                'type':'KBaseSets.ReadsSet',
+                                'data':readsSet_obj,
+                                'name':name,
+                                'meta':{},
+                                'provenance':[
+                                    {
+                                        'service':'kb_util_dylan',
+                                        'method':'test_kb_util_dylan'
+                                    }
+                                ]
+                            }]
+                        })[0]
+
+        # store it
+        self.__class__.singleEndLib_SetInfo = new_obj_info
+        return new_obj_info
+
+
+    # call this method to get the WS object info of a Paired End Library Set (will
+    # upload the example data if this is the first time the method is called during tests)
+    def getPairedEndLib_SetInfo(self, read_libs_basename_list, refresh=False):
+        if hasattr(self.__class__, 'pairedEndLib_SetInfo'):
+            try:
+                info = self.__class__.pairedEndLib_SetInfo
+                if info != None:
+                    if refresh:
+                        self.__class__.pairedEndLib_SetInfo[lib_i] = None
+                    else:
+                        return info
+            except:
+                pass
+
+        # build items and save each PairedEndLib
+        items = []
+        for lib_i,read_lib_basename in enumerate (read_libs_basename_list):
+            label    = read_lib_basename
+            lib_info = self.getPairedEndLibInfo (read_lib_basename, lib_i)
+            lib_ref  = str(lib_info[6])+'/'+str(lib_info[0])
+            print ("LIB_REF["+str(lib_i)+"]: "+lib_ref+" "+read_lib_basename)  # DEBUG
+
+            items.append({'ref': lib_ref,
+                          'label': label
+                          #'data_attachment': ,
+                          #'info':
+                         })
+
+        # save readsset
+        desc = 'test ReadsSet'
+        readsSet_obj = { 'description': desc,
+                         'items': items
+                       }
+        name = 'TEST_READSET'
+
+        new_obj_info = self.wsClient.save_objects({
+                        'workspace':self.getWsName(),
+                        'objects':[
+                            {
+                                'type':'KBaseSets.ReadsSet',
+                                'data':readsSet_obj,
+                                'name':name,
+                                'meta':{},
+                                'provenance':[
+                                    {
+                                        'service':'kb_util_dylan',
+                                        'method':'test_kb_util_dylan'
+                                    }
+                                ]
+                            }]
+                        })[0]
+
+        # store it
+        self.__class__.pairedEndLib_SetInfo = new_obj_info
+        return new_obj_info
+
+
+    ##############
+    # UNIT TESTS #
+    ##############
+
     def test_KButil_Split_Reads(self):
-        # Prepare test objects in workspace if needed using 
-        # self.getWsClient().save_objects({'workspace': self.getWsName(), 'objects': []})
-        #
-        # Run your method by
-        # ret = self.getImpl().your_method(self.getContext(), parameters...)
-        #
-        # Check returned data with
-        # self.assertEqual(ret[...], ...) or other unittest methods
+
+        print ("\n\nRUNNING: test_KButil_Split_Reads()")
+        print ("==================================\n\n")
 
         # figure out where the test data lives
-        pe_lib_info = self.getPairedEndLibInfo()
+        pe_lib_info = self.getPairedEndLibInfo('small_2')
         pprint(pe_lib_info)
 
         # Object Info Contents
@@ -206,11 +313,12 @@ class kb_util_dylanTest(unittest.TestCase):
 
         # run Split_Reads
         split_num = 4
+        base_output_name = 'output'
         params = {
             'workspace_name': pe_lib_info[7],
-            'input_name': pe_lib_info[1],
+            'input_ref': str(pe_lib_info[6])+'/'+str(pe_lib_info[0]),
             'split_num': split_num,
-            'output_name': 'output',
+            'output_name': base_output_name,
             'desc':'test split',
             #'k_min':31,
             #'k_max':51,
@@ -224,35 +332,18 @@ class kb_util_dylanTest(unittest.TestCase):
         pprint(result)
 
         # check the output
-        output_name = 'output_paired-0'
+        output_name = base_output_name+'_paired-0'
         output_type = 'KBaseFile.PairedEndLibrary'
-        info_list = self.wsClient.get_object_info([{'ref':pe_lib_info[7] + '/' + output_name}], 1)
+        info_list = self.getWsClient().get_object_info_new({'objects':[{'ref':pe_lib_info[7] + '/' + output_name}]})
         self.assertEqual(len(info_list),1)
         readsLib_info = info_list[0]
         self.assertEqual(readsLib_info[1],output_name)
         self.assertEqual(readsLib_info[2].split('-')[0],output_type)
 
-        output_name = 'output_paired-'+str(split_num)
+        output_name = base_output_name+'_paired-'+str(split_num-1)
         output_type = 'KBaseFile.PairedEndLibrary'
-        info_list = self.wsClient.get_object_info([{'ref':pe_lib_info[7] + '/' + output_name}], 1)
+        info_list = self.getWsClient().get_object_info_new({'objects':[{'ref':pe_lib_info[7] + '/' + output_name}]})
         self.assertEqual(len(info_list),1)
         readsLib_info = info_list[0]
         self.assertEqual(readsLib_info[1],output_name)
         self.assertEqual(readsLib_info[2].split('-')[0],output_type)
-
-        output_name = 'output_unpaired-fwd'
-        output_type = 'KBaseFile.SingleEndLibrary'
-        info_list = self.wsClient.get_object_info([{'ref':pe_lib_info[7] + '/' + output_name}], 1)
-        self.assertEqual(len(info_list),1)
-        readsLib_info = info_list[0]
-        self.assertEqual(readsLib_info[1],output_name)
-        self.assertEqual(readsLib_info[2].split('-')[0],output_type)
-
-        output_name = 'output_unpaired-rev'
-        output_type = 'KBaseFile.SingleEndLibrary'
-        info_list = self.wsClient.get_object_info([{'ref':pe_lib_info[7] + '/' + output_name}], 1)
-        self.assertEqual(len(info_list),1)
-        readsLib_info = info_list[0]
-        self.assertEqual(readsLib_info[1],output_name)
-        self.assertEqual(readsLib_info[2].split('-')[0],output_type)
-
